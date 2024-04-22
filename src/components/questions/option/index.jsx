bd902378867432cf037af7ddceca0d1ea5f7826a
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-22 17:36:25 +0200

    Added conditional classnames

--- src/components/questions/option/index.jsx
+++ src/components/questions/option/index.jsx
@@ -2,8 +2,20 @@ export default function Option({ question, answer, dispatch }) {
     return (
         <div>
             <div className="options">
-                {question.options.map((option) => (
-                    <button className="btn btn-option" key={option}>
+                {question.options.map((option, index) => (
+                    <button
+                        className={`btn btn-option ${
+                            index === answer ? "answer" : ""
+                        } ${
+                            index === question.correctOption
+                                ? "correct"
+                                : "wrong"
+                        }`}
+                        key={option}
+                        onClick={() =>
+                            dispatch({ type: "newAnswer", payload: index })
+                        }
+                    >
                         {option}
                     </button>
                 ))}
