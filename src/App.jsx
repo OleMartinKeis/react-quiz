3142a4c959a2e30679a81b8df49f1b9315977169
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-22 13:01:00 +0200

    Added API call to our JSON server

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 // import DateCounter from "./components/dateCounter/dateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./components/header/header";
 import Main from "./components/main";
 
+const initialState = {
+    questions: [],
+    // "loading, "error", "ready", "active", "finished"
+    status: "loading",
+};
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready",
+            };
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error",
+            };
+        default:
+            throw new Error("action unknown");
+    }
+}
+
 function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    useEffect(function () {
+        fetch("http://localhost:8000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
+            .catch((err) => dispatch({ type: "dataFailed" }));
+    }, []);
     return (
         <div className="app">
             <Header />
