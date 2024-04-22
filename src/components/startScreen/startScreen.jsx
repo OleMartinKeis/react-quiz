b981f407bdde225c0e50b7699823b9537c4b41c9
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-22 13:23:21 +0200

    Update startScreen.jsx

--- src/components/startScreen/startScreen.jsx
+++ src/components/startScreen/startScreen.jsx
@@ -3,7 +3,7 @@ export default function StartScreen({ numQuestions }) {
         <div>
             <h2>Welcome to The React Quiz!</h2>
             <h3>{numQuestions} questions to test your React mastery</h3>
-            <button className="btn btn-ui">Let's start</button>
+            <button className="btn btn-ui">Let&apos;s start</button>
         </div>
     );
 }
