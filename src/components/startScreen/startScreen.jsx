131a5476263574ba103122cbe4fd3c6218bbcf5c
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-22 13:29:33 +0200

    Added a start action.type to start quiz

--- src/components/startScreen/startScreen.jsx
+++ src/components/startScreen/startScreen.jsx
@@ -3,7 +3,12 @@ export default function StartScreen({ numQuestions, dispatch }) {
         <div>
             <h2>Welcome to The React Quiz!</h2>
             <h3>{numQuestions} questions to test your React mastery</h3>
-            <button className="btn btn-ui">Let&apos;s start</button>
+            <button
+                className="btn btn-ui"
+                onClick={() => dispatch({ type: "start" })}
+            >
+                Let&apos;s start
+            </button>
         </div>
     );
 }
