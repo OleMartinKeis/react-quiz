37e97a343193b03e89c35393dd98558e7c9658ad
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 14:31:45 +0200

    Added classname for styling

--- src/components/finishScreen/index.jsx
+++ src/components/finishScreen/index.jsx
@@ -2,7 +2,7 @@ export default function FinishScreen({ maxPossiblePoints, points }) {
     // Finds the percentage of how many points you have scored
     const percentage = (points / maxPossiblePoints) * 100;
     return (
-        <p>
+        <p className="result">
             You scored <strong>{points}</strong> out of {maxPossiblePoints} (
             {Math.ceil(percentage)})
         </p>
