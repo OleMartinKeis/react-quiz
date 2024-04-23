51839095a1814fbb216a9cf550ffb15e88b7f9ae
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 13:02:50 +0200

    Added points to prgressbar

--- src/components/progress/index.jsx
+++ src/components/progress/index.jsx
@@ -1,11 +1,13 @@
-export default function Progress({ index, numQuestions }) {
+export default function Progress({ index, numQuestions, points }) {
     return (
         <header className="progress">
             <p>
                 Question <strong>{index + 1}</strong> / {numQuestions}
             </p>
             <p></p>
-            <p></p>
+            <p>
+                <strong>{points}</strong> / x
+            </p>
         </header>
     );
 }
