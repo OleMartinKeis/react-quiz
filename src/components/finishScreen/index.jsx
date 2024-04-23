347101f62a38857b31540681056233d73002c138
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 14:18:34 +0200

    Update index.jsx
    Finds the percentage of how many points you have scored and displays it on the finish screen

--- src/components/finishScreen/index.jsx
+++ src/components/finishScreen/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-
 export default function FinishScreen({ maxPossiblePoints, points }) {
+    // Finds the percentage of how many points you have scored
+    const percentage = (points / maxPossiblePoints) * 100;
     return (
         <p>
-            You scored <strong>{points}</strong> of {maxPossiblePoints}
+            You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+            {Math.ceil(percentage)})
         </p>
     );
 }
