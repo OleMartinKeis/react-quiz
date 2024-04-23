1d4b680683edcb3de41009c6f409f9c97ff50598
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 14:49:37 +0200

    If on the final question, display finish instead

--- src/components/nextButton/index.jsx
+++ src/components/nextButton/index.jsx
@@ -1,5 +1,6 @@
 export default function NextButton({ dispatch, answer, index, numQuestions }) {
     if (answer === null) return null;
+
     if (index < numQuestions - 1)
         return (
             <button
@@ -7,6 +8,16 @@ export default function NextButton({ dispatch, answer, index, numQuestions }) {
                 onClick={() => dispatch({ type: "nextQuestion" })}
             >
                 Next
+            </button>
+        );
+
+    if (index === numQuestions - 1)
+        return (
+            <button
+                className="btn btn-ui"
+                onClick={() => dispatch({ type: "finish" })}
+            >
+                Finish
             </button>
         );
 }
