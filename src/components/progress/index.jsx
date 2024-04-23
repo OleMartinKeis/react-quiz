c590785fc59005305c93070fb78237dad74b0670
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 12:54:27 +0200

    Create progress bar

---
+++ src/components/progress/index.jsx
@@ -0,0 +1,9 @@
+export default function Progress() {
+    return (
+        <header className="progress">
+            <p></p>
+            <p></p>
+            <p></p>
+        </header>
+    );
+}
