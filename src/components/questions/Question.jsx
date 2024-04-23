51f75804ac57fba9c5e237788dd73365d66484b7
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-23 14:21:24 +0200

    Removed console log from Question

--- src/components/questions/Question.jsx
+++ src/components/questions/Question.jsx
@@ -1,7 +1,6 @@
 import Option from "./option";
 
 export default function Question({ question, answer, dispatch }) {
-    console.log(question);
     return (
         <div>
             <h4>{question.question}</h4>
